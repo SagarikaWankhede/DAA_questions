c07ead334471447ed7bf39f5701efd0647071431
SagarikaWankhede <sagarika.wankhede@gmail.com>
2026-01-22 14:29:37 +0530

    ques4

--- Assignment1/a1_ques4.cpp
+++ Assignment1/a1_ques4.cpp
@@ -1,18 +1,59 @@
-#include<iostream>
+#include <iostream>
+#include <climits>
 using namespace std;
-int maxSubArraySum(int arr[], int n){
-    int maxSoFar= arr[0];
-    int currentSum= arr[0];
-    for (int i=1; i<n; i++){
-        currentSum= max(arr[i], currentSum+arr[i]);
-        maxSoFar= max(maxSoFar, currentSum);
+
+int maxCrossingSum(int arr[], int left, int mid, int right)
+{
+    int sum = 0;
+    int leftSum = INT_MIN;
+
+    for (int i = mid; i >= left; i--)
+    {
+        sum += arr[i];
+        if (sum > leftSum)
+            leftSum = sum;
     }
-    return maxSoFar;
+
+    sum = 0;
+    int rightSum = INT_MIN;
+
+    for (int i = mid + 1; i <= right; i++)
+    {
+        sum += arr[i];
+        if (sum > rightSum)
+            rightSum = sum;
+    }
+
+    return leftSum + rightSum;
 }
-int main(){
-    int arr[]={-2,-5,6,-2,-3,1,5,-6};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    cout<<"maximum subarray sum= "<<maxSubArraySum(arr, n)<<endl;
+
+int maxSubArraySum(int arr[], int left, int right)
+{
+    if (left == right)
+        return arr[left];
+
+    int mid = (left + right) / 2;
+
+    int leftMax = maxSubArraySum(arr, left, mid);
+    int rightMax = maxSubArraySum(arr, mid + 1, right);
+    int crossMax = maxCrossingSum(arr, left, mid, right);
+
+    if (leftMax >= rightMax && leftMax >= crossMax)
+        return leftMax;
+    else if (rightMax >= leftMax && rightMax >= crossMax)
+        return rightMax;
+    else
+        return crossMax;
+}
+
+int main()
+{
+    int arr[] = {-2, -5, 6, -2, -3, 1, 5, -6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int maxSum = maxSubArraySum(arr, 0, n - 1);
+
+    cout << "Maximum subarray sum is: " << maxSum;
+
     return 0;
-    
 }
